84a2ed2006d02142677307cbe658d2529fd495f4
Jeya Karthika <hello@jeyakarthika.com>
2014-02-04 10:13:27 +1000

    quiz folder structure updated

---
+++ projects/ColourQuiz/function/action.js
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------
+
+	Project Name: Colour that Name
+	Project Description: A Thinkful Quiz App
+	File Name: action.js
+	Author: Jeya Karthika
+	Author URI: http://www.freshsqueaks.com
+	Version: 0.1
+	
+----------------------------------------------------------------------------------*/
+
+$(document).ready(function(){
+	
+});
+
