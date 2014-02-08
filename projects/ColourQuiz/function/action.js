711f6ac3f28e89bcfc696a59b241866921c8866a
Jeya Karthika <hello@jeyakarthika.com>
2014-02-08 18:23:50 +1000

    quiz html and css

--- projects/ColourQuiz/function/action.js
+++ projects/ColourQuiz/function/action.js
@@ -1,15 +1,31 @@
 /*---------------------------------------------------------------------------------
 
-	Project Name: Colour that Name
-	Project Description: A Thinkful Quiz App
+	Project Name: todo
+	Project Description: A Thinkful Shopping List App
 	File Name: action.js
 	Author: Jeya Karthika
 	Author URI: http://www.freshsqueaks.com
-	Version: 0.1
+	Version: 2.0
 	
 ----------------------------------------------------------------------------------*/
 
 $(document).ready(function(){
-	
+
+	var doCount = 3;
+	var doneCount = 1;
+	updateCount();
+
+	/*--- Clear and Set focus to the inputbox ---*/
+	function setFocus() {
+		$('#newItem').val('');
+		document.getElementById("newItem").focus();
+	}
+
+	/*--- Update the DO, DONE & TOTAL counts ---*/
+	function updateCount() {
+		$('#do').text(doCount);
+		$('#done').text(doneCount);
+		$('#total').text(doCount + doneCount);
+	}
 });
 
