f385c5f4ffc0cb389dccc8bc39f64f2b1e57dc03
Jeya Karthika <hello@jeyakarthika.com>
2014-03-14 20:27:28 +1000

    works

--- projects/Portfolio/function/action.js
+++ projects/Portfolio/function/action.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	
 	//full page sliders
 	$.fn.fullpage({
-				slidesColor: ['#FECC00', '#2C2D31', '#FC575F', '#33C1C9']
+				slidesColor: ['#FECC00', '#D3C67B', '#FC575F', '#33C1C9']
 			});
 
 	
